5a072ab0bc365fca134ee8f4a796e32e4213cf01
Dave <dave.e.siegel@gmail.com>
2020-02-07 15:43:11 -0500

    refactored CreateTextFile to make getting column headers more concise

--- src/server/utils/CreateTextFile.ts
+++ src/server/utils/CreateTextFile.ts
@@ -1,8 +1,6 @@
 import { Product } from './InterfacesAndTypes';
 export default (catalog: Product[]): string => {
-  const headers: string[] = [];
-  const firstRow: Product = catalog[0];
-  firstRow.forEach((value: string, key: string) => headers.push(key));
+  const headers: string[] = [...catalog[0].keys()];
   const body: string = catalog.reduce(
     (str: string, product: Product): string => {
       product.forEach((value: string) => (str += `${value}\t`));
